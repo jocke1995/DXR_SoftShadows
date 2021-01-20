d7f52e8e39e2b49b48c37d731819aa9f506a7b5a
Filip Zachrisson <fjz345@hotmail.com>
2021-01-20 11:19:53 +0100

    Working commandline arguments

--- DXR_SoftShadows_Project/Sandbox/src/main.cpp
+++ DXR_SoftShadows_Project/Sandbox/src/main.cpp
@@ -6,9 +6,76 @@ Scene* SponzaScene(SceneManager* sm);
 void TestUpdateScene(SceneManager* sm, double dt);
 void SponzaUpdateScene(SceneManager* sm, double dt);
 
+
+struct ApplicationParameters
+{
+    std::wstring scene = L"test";
+    std::wstring outputFile = L"results.csv";
+    bool useInlineRaytracing = false;
+};
+
+bool parseParameters(ApplicationParameters* output)
+{
+    LPWSTR* szArglist;
+    int nArgs;
+
+    szArglist = CommandLineToArgvW(GetCommandLineW(), &nArgs);
+    if (NULL == szArglist)
+    {
+        Log::Print("CommandLineToArgvW failed\n");
+        return false;
+    }
+    else
+    {
+        for (int i = 0; i < nArgs; i++)
+        {
+            Log::Print("%d: %ws\n", i, szArglist[i]);
+
+            auto a = szArglist[i];
+            // Scene
+            if (wcscmp(szArglist[i], L"-s") == 0)
+            {
+                output->scene = szArglist[++i];
+            }
+
+            // Result file
+            if (wcscmp(szArglist[i], L"-o") == 0)
+            {
+                output->outputFile = szArglist[++i];
+            }
+
+            // Inline Raytracing
+            if (wcscmp(szArglist[i], L"-i") == 0)
+            {
+                ++i;
+
+                if (wcscmp(szArglist[i], L"true") == 0 || wcscmp(szArglist[i], L"True") == 0 || wcscmp(szArglist[i], L"TRUE") == 0 ||
+                    wcscmp(szArglist[i], L"yes") == 0 || wcscmp(szArglist[i], L"Yes") == 0 || wcscmp(szArglist[i], L"YES") == 0 || 
+                    wcscmp(szArglist[i], L"1") == 0)
+                {
+                    output->useInlineRaytracing = true;
+                }
+                else
+                {
+                    output->useInlineRaytracing = false;
+                }
+            }
+        }
+    }
+
+    // Free memory allocated by CommandLineToArgvW
+    LocalFree(szArglist);
+    
+    return true;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+    /* ------ Command line arguments  ------ */
+    ApplicationParameters params;
+    parseParameters(&params);
 
     /* ------ Engine  ------ */
     BeLuEngine engine;
@@ -24,24 +91,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
     /*------ AssetLoader to load models / textures ------*/
    AssetLoader* al = AssetLoader::Get();
    
-   //Scene* scene = SponzaScene(sceneManager);
-   Scene* scene = TestScene(sceneManager);
+   Scene* scene;
+   if (params.scene == L"test2")
+   {
+        scene = TestScene(sceneManager);
+   }
+   else
+   {
+       scene = SponzaScene(sceneManager);
+   }
+   
    
    // Set scene
    sceneManager->SetScene(scene);
 
-
-   LPWSTR* szArglist;
-   int nArgs;
-   int i;
-
-   szArglist = CommandLineToArgvW(GetCommandLineW(), &nArgs);
-   if (NULL == szArglist)
-   {
-       Log::Print("CommandLineToArgvW failed\n");
-       return 0;
-   }
-   else for (i = 0; i < nArgs; i++) Log::Print("%d: %ws\n", i, szArglist[i]);
+   
+   
 
    
    Log::Print("Entering Game-Loop ...\n\n");
