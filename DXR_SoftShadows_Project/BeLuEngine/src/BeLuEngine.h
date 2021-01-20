da8092b86f5735d965d75550342830796d192f21
Filip Zachrisson <fjz345@hotmail.com>
2021-01-20 11:28:52 +0100

    Moved to h file

--- DXR_SoftShadows_Project/BeLuEngine/src/BeLuEngine.h
+++ DXR_SoftShadows_Project/BeLuEngine/src/BeLuEngine.h
@@ -5,6 +5,7 @@
 #include "Misc/Window.h"
 #include "Misc/Timer.h"
 #include "Misc/AssetLoader.h"
+#include "Misc/ApplicationParameters.h"
 
 // Entity Component System
 #include "ECS/SceneManager.h"
--- DXR_SoftShadows_Project/Sandbox/src/main.cpp
+++ DXR_SoftShadows_Project/Sandbox/src/main.cpp
@@ -7,67 +7,7 @@ void TestUpdateScene(SceneManager* sm, double dt);
 void SponzaUpdateScene(SceneManager* sm, double dt);
 
 
-struct ApplicationParameters
-{
-    std::wstring scene = L"test";
-    std::wstring outputFile = L"results.csv";
-    bool useInlineRaytracing = false;
-};
 
-bool parseParameters(ApplicationParameters* output)
-{
-    LPWSTR* szArglist;
-    int nArgs;
-
-    szArglist = CommandLineToArgvW(GetCommandLineW(), &nArgs);
-    if (NULL == szArglist)
-    {
-        Log::Print("CommandLineToArgvW failed\n");
-        return false;
-    }
-    else
-    {
-        for (int i = 0; i < nArgs; i++)
-        {
-            Log::Print("%d: %ws\n", i, szArglist[i]);
-
-            auto a = szArglist[i];
-            // Scene
-            if (wcscmp(szArglist[i], L"-s") == 0)
-            {
-                output->scene = szArglist[++i];
-            }
-
-            // Result file
-            if (wcscmp(szArglist[i], L"-o") == 0)
-            {
-                output->outputFile = szArglist[++i];
-            }
-
-            // Inline Raytracing
-            if (wcscmp(szArglist[i], L"-i") == 0)
-            {
-                ++i;
-
-                if (wcscmp(szArglist[i], L"true") == 0 || wcscmp(szArglist[i], L"True") == 0 || wcscmp(szArglist[i], L"TRUE") == 0 ||
-                    wcscmp(szArglist[i], L"yes") == 0 || wcscmp(szArglist[i], L"Yes") == 0 || wcscmp(szArglist[i], L"YES") == 0 || 
-                    wcscmp(szArglist[i], L"1") == 0)
-                {
-                    output->useInlineRaytracing = true;
-                }
-                else
-                {
-                    output->useInlineRaytracing = false;
-                }
-            }
-        }
-    }
-
-    // Free memory allocated by CommandLineToArgvW
-    LocalFree(szArglist);
-    
-    return true;
-}
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
@@ -75,7 +15,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 
     /* ------ Command line arguments  ------ */
     ApplicationParameters params;
-    parseParameters(&params);
+    ParseParameters(&params);
 
     /* ------ Engine  ------ */
     BeLuEngine engine;
