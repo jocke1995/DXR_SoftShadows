bb2a882f12d206e8860bf996b136413d7678bcb8
Joakim <joakim.sjo@gmail.com>
2021-03-03 19:13:02 +0100

    Hardshadows with inline raytracing from the pixelshader working

--- DXR_SoftShadows_Project/BeLuEngine/src/Renderer/HLSL/ForwardPixel.hlsl
+++ DXR_SoftShadows_Project/BeLuEngine/src/Renderer/HLSL/ForwardPixel.hlsl
@@ -18,19 +18,12 @@ ByteAddressBuffer rawBufferLights: register(t0, space3);
 ConstantBuffer<CB_PER_OBJECT_STRUCT> cbPerObject : register(b1, space3);
 ConstantBuffer<CB_PER_FRAME_STRUCT>  cbPerFrame  : register(b4, space3);
 
-// Raytracing acceleration structure, accessed as a SRV
-RaytracingAccelerationStructure SceneBVH : register(t0, space4);
-
-
 PS_OUTPUT PS_main(VS_OUT input)
 {
 	// Sample from texture
 	float4 albedo   = textures[cbPerObject.info.textureAlbedo	].Sample(Anisotropic16_Wrap, input.uv);
 
-	//normal = (2.0f * normal) - 1.0f;
-	//normal = float4(normalize(mul(normal.xyz, input.tbn)), 1.0f);
-
-	float3 finalColor = float3(1.0f, 0.0f, 0.0f);
+	float3 finalColor = float3(0.0f, 0.0f, 0.0f);
 
 	// PointLight Test
 	LightHeader lHeader = rawBufferLights.Load<LightHeader>(0);
@@ -41,6 +34,7 @@ PS_OUTPUT PS_main(VS_OUT input)
 		finalColor += CalcPointLight(pl, input.worldPos, albedo, input.norm);
 	}
 
+	finalColor += albedo * float3(0.1f, 0.1f, 0.1f);
 	PS_OUTPUT output;
 	output.sceneColor = float4(finalColor.rgb, 1.0f);
 	return output;
--- DXR_SoftShadows_Project/BeLuEngine/src/Renderer/HLSL/LightCalculations.hlsl
+++ DXR_SoftShadows_Project/BeLuEngine/src/Renderer/HLSL/LightCalculations.hlsl
@@ -11,16 +11,51 @@ SamplerState samplerTypeBorder	: register (s4);
 
 ConstantBuffer<CB_PER_SCENE_STRUCT>  cbPerScene  : register(b5, space3);
 
+// Raytracing acceleration structure, accessed as a SRV
+RaytracingAccelerationStructure SceneBVH : register(t0, space4);
+
+float RT_ShadowFactor(float3 worldPos, float3 lightDir)
+{
+	RayQuery<RAY_FLAG_CULL_NON_OPAQUE | RAY_FLAG_SKIP_PROCEDURAL_PRIMITIVES | RAY_FLAG_ACCEPT_FIRST_HIT_AND_END_SEARCH> q;
+	
+	uint rayFlags = 0;
+	uint instanceMask = 0xff;
+	
+	float shadowFactor = 1.0f;
+	
+	RayDesc ray = (RayDesc)0;
+	ray.TMin = 0.01f;
+	ray.TMax = length(lightDir);
+	
+	ray.Direction = normalize(lightDir);
+	ray.Origin = worldPos;
+
+	q.TraceRayInline(
+		SceneBVH,
+		rayFlags,
+		instanceMask,
+		ray
+	);
+
+	q.Proceed();
+
+	if (q.CommittedStatus() == COMMITTED_TRIANGLE_HIT)
+	{
+		shadowFactor = 0.0f;
+	}
+
+	return shadowFactor;
+}
+
 float3 CalcPointLight(
 	in PointLight pointLight,
-	in float4 fragPos,
+	in float4 worldPos,
 	in float3 albedo,
 	in float3 normal)
 {
 	// Basic Diffuse lightning
-	float3 pointLightContribution = float3(0.0f, 0.0f, 0.0f);
-	float3 lightDir = normalize(pointLight.position - fragPos.xyz);
-	float NdotL = max(dot(normal, lightDir), 0.0f);
+	float3 lightDir = pointLight.position - worldPos.xyz;
+	float NdotL = max(dot(normal, normalize(lightDir)), 0.0f);
 
-	return pointLight.baseLight.color * NdotL * albedo;
+	return albedo * pointLight.baseLight.color * NdotL * RT_ShadowFactor(worldPos.xyz, lightDir);
 }
