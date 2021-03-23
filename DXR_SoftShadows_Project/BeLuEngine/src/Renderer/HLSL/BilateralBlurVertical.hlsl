118e5cb65c86cee601653ee4b6bc7853676e891e
Joakim <joakim.sjo@gmail.com>
2021-03-23 19:23:17 +0100

    Bug

--- DXR_SoftShadows_Project/BeLuEngine/src/Renderer/HLSL/BilateralBlurVertical.hlsl
+++ DXR_SoftShadows_Project/BeLuEngine/src/Renderer/HLSL/BilateralBlurVertical.hlsl
@@ -11,33 +11,20 @@ SamplerState MIN_MAG_MIP_LINEAR__WRAP : register(s5);
 
 static const int g_BlurRadius = 4;
 static const int g_NumThreads = 256;
+// TODO: dont hardcode screensize
+static const float2 screenSize = float2(1280, 720);
+
 groupshared float4 g_SharedMem[g_NumThreads + 2 * g_BlurRadius];
-
-// Calculate world pos from DepthBuffer
-float3 WorldPosFromDepth(float depth, float2 TexCoord)
-{
-	TexCoord.y = 1.0 - TexCoord.y;
-	float4 clipSpacePosition = float4(TexCoord * 2.0 - 1.0, depth, 1.0);
-	float4 viewSpacePosition = mul(cbCameraMatrices.projectionI, clipSpacePosition);
-
-	// Perspective division
-	viewSpacePosition /= viewSpacePosition.w;
-
-	float4 worldSpacePosition = mul(cbCameraMatrices.viewI, viewSpacePosition);
-
-	return worldSpacePosition.xyz;
-}
 
 [numthreads(1, g_NumThreads, 1)]
 void CS_main(uint3 dispatchThreadID : SV_DispatchThreadID, int3 groupThreadID : SV_GroupThreadID)
 {
 	// TODO: dont hardcode screensize
-	float2 uv = dispatchThreadID.xy / float2(1280, 720);
+	float2 uv = dispatchThreadID.xy / screenSize;
 	
 	/* Sample depth and normal from textures */
 	float depth = textures[cbPerScene.depthBufferIndex].SampleLevel(MIN_MAG_MIP_LINEAR__WRAP, uv, 0).r;
 	float3 normal = textures[cbPerScene.gBufferNormalIndex].SampleLevel(MIN_MAG_MIP_LINEAR__WRAP, uv, 0).rgb;
-	float3 worldPos = WorldPosFromDepth(depth, uv);
 
 	/* DescriptorHeap indices */
 	unsigned int readIndex = dhIndices.index2;
@@ -75,14 +62,34 @@ void CS_main(uint3 dispatchThreadID : SV_DispatchThreadID, int3 groupThreadID : 
 	// Blur
 	// Current fragments contribution
 	float4 blurColor = weights[0] * g_SharedMem[groupThreadID.y + g_BlurRadius];
+	float totalWeight = weights[0];
 
 	// Adjacent fragment contributions
 	for (int i = 1; i <= g_BlurRadius; i++)
 	{
+		// Top side
+		float2 uvTop = (dispatchThreadID.xy - float2(0, i)) / screenSize;
+		float depthTop = textures[cbPerScene.depthBufferIndex].SampleLevel(MIN_MAG_MIP_LINEAR__WRAP, uvTop, 0).r;
+		float3 normalTop = textures[cbPerScene.gBufferNormalIndex].SampleLevel(MIN_MAG_MIP_LINEAR__WRAP, uvTop, 0).rgb;
+
 		int top = groupThreadID.y + g_BlurRadius - i;
+		if (dot(normalTop, normal) >= 0.8f && abs(depthTop - depth) <= 0.2f)	// Skip pixels if the neighbor values differ to much
+		{
+			blurColor += weights[i] * g_SharedMem[top];
+			totalWeight += weights[i];
+		}
+
+		// Bot side
+		float2 uvBot = (dispatchThreadID.xy + float2(0, i)) / screenSize;
+		float depthBot = textures[cbPerScene.depthBufferIndex].SampleLevel(MIN_MAG_MIP_LINEAR__WRAP, uvBot, 0).r;
+		float3 normalBot = textures[cbPerScene.gBufferNormalIndex].SampleLevel(MIN_MAG_MIP_LINEAR__WRAP, uvBot, 0).rgb;
+
 		int bot = groupThreadID.y + g_BlurRadius + i;
-		blurColor += weights[i] * g_SharedMem[top];
-		blurColor += weights[i] * g_SharedMem[bot];
+		if (dot(normalBot, normal) >= 0.8f && abs(depthBot - depth) <= 0.2f)	// Skip pixels if the neighbor values differ to much
+		{
+			blurColor += weights[i] * g_SharedMem[bot];
+			totalWeight += weights[i];
+		}
 	}
 
 	textureToBlur[writeIndex][dispatchThreadID.xy] = blurColor;
